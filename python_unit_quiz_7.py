290e745ae6494b696175805b456d60fe9510e822
WaltWalt2 <130768136+WaltWalt2@users.noreply.github.com>
2023-04-28 09:33:59 -0400

    Add files via upload

---
+++ python_unit_quiz_7.py
@@ -0,0 +1,18 @@
+# 1. A block of code which only runs when it is called. A thing would be where you would want to see if something is done right so you run the function to see if it works.
+# 2. The method operates the data in the class, while a function is used to return or pass the data.
+# 3. The dictionary is an unordered collection that contains key:value pairs separated by commas inside curly brackets. If you want to help define a certain food.
+# 4. GitHub is a code hosting platform for version control and collaboration. You can share your ideas with people, You can show them what you're coding on, and you can see others coding.
+# 5. Classes are for holding data together while dictionaries are to keep hold of key-value relationships.
+# 6.
+city_names = ('Tokyo, New York City, Venice, Dubai, Berlin')
+
+def city():
+    print(city_names)
+
+# 7.
+# 8.
+# 9.
+# 10. A misplaced letter in the code, improper text, or a significant missing piece
+# 11. There's no quotation marks and there's no quotations around the word name.
+def function(name)
+    print(hello, name. How are you today?)
